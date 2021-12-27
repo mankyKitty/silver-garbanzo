3626583571757a95f3243904d0d519e1e83cf03b
Sean Chalmers <sean.chalmers@obsidian.systems>
2021-12-27 13:24:23 +0000

    Integrate idris2-pkgs

--- flake.nix
+++ flake.nix
@@ -6,31 +6,51 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    idris2-pkgs.url = "github:claymager/idris2-pkgs";
+    nixpkgs.follows = "idris2-pkgs/nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, idris2-pkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
       let
-        overlays = [ ];
-        pkgs =
-          import nixpkgs { inherit system overlays; config.allowBroken = true; };
+        overlays = [
+          idris2-pkgs.overlay
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowBroken = true;
+        };
+
+        inherit (pkgs.idris2-pkgs._builders) idrisPackage devEnv;
+
         idrisPkgs =
           if system == "aarch64-darwin"
           then import nixpkgs { system = "x86_64-darwin"; }  # Rosetta only, no M1 build available
           else pkgs;
+
+        mypkg = idrisPackage ./. { }; # this package
+        runTests = idrisPackage ./tests {
+          extraPkgs.mypkg = mypkg;
+        };
+
         project = returnShellEnv:
           pkgs.mkShell {
-            packages = with pkgs; [
-              idrisPkgs.idris2
-              nixpkgs-fmt
-              nodePackages.live-server
-              goreman
-              entr
-              nodejs
-              nodePackages.concurrently
+            buildInputs = [
+              (devEnv mypkg)
             ];
           };
       in
       {
+        packages = with pkgs; [
+          mypkg
+          runTests
+          idrisPkgs.idris2
+          nixpkgs-fmt
+          nodePackages.live-server
+          goreman
+          entr
+          nodejs
+          nodePackages.concurrently
+        ];
         # Used by `nix build` & `nix run` (prod exe)
         defaultPackage = project false;
 
